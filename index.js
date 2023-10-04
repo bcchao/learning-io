e8016e5461d4e466b5c6d6afe44629ca932c4d33
Jesus Paulino <jesuspaulino17@outlook.com>
2023-10-04 18:34:36 -0400

    no perfomance issues ;)

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const plonkButton = document.querySelector("#plonkButton");
 const plinkContainer = document.querySelector(".plinkContainer");
 function onPlinkClick() {
   console.log("Plink!");
-  plinkButton.innerText = plinkButton.innerText === "Plink" ? "Unplink" : "Plink";
+  plinkButton.textContent = plinkButton.textContent === "Plink" ? "Unplink" : "Plink";
   plinkContainer.style.display =
     plinkContainer.style.display === "block" ? "none" : "block";
 }
 const plonkContainer = document.querySelector(".plonkContainer");
 function onPlonkClick() {
   console.log("Plonk!");
-  plonkButton.innerText = plonkButton.innerText === "Plonk" ? "Unplonk" : "Plonk";
+  plonkButton.textContent = plonkButton.textContent === "Plonk" ? "Unplonk" : "Plonk";
   plonkContainer.style.display =
     plonkContainer.style.display === "block" ? "none" : "block";
 }
