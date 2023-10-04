82033cfb9034334fec1299e5043288680a3172a6
Jesus Paulino <jesuspaulino17@outlook.com>
2023-10-04 18:33:05 -0400

    Buttons unplink and unplonk now

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+const plinkButton = document.querySelector("#plinkButton");
+const plonkButton = document.querySelector("#plonkButton");
 const plinkContainer = document.querySelector(".plinkContainer");
 function onPlinkClick() {
   console.log("Plink!");
+  plinkButton.innerText = plinkButton.innerText === "Plink" ? "Unplink" : "Plink";
   plinkContainer.style.display =
     plinkContainer.style.display === "block" ? "none" : "block";
 }
 const plonkContainer = document.querySelector(".plonkContainer");
 function onPlonkClick() {
   console.log("Plonk!");
+  plonkButton.innerText = plonkButton.innerText === "Plonk" ? "Unplonk" : "Plonk";
   plonkContainer.style.display =
     plonkContainer.style.display === "block" ? "none" : "block";
 }
